6363f3e7b382571545734ac042fb2e15d061c095
Vinigperuzzi <vinigperuzzi@hotmail.com>
2023-02-27 01:26:04 -0300

    aula 07 feita

---
+++ Aula05-JS-basico/index.js
@@ -0,0 +1,8 @@
+var x = 5;
+
+console.log('Olá, Mundo!', x, 'Foi mesmo');
+console.log(x);
+
+alert('Olá, Mundo!');
+
+console.log("No node isso nao chega a ser exibido por causa do erro");
---
+++ aula07-constantes/index.js
@@ -0,0 +1,12 @@
+const nome = 'João';
+console.log(nome); 
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+console.log(resultado);
+const resultadoDuplicado = resultado * 2;
+console.log(resultadoDuplicado);
+
+console.log(typeof(nome));
+console.log(typeof(primeiroNumero));
